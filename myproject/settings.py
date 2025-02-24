31ead68440ad1740e19ee377beaa9e7f4bd3feb9
F43Z <50200066+faeztgh@users.noreply.github.com>
2025-02-25 00:53:39 +0330

    allowed hosts

--- myproject/settings.py
+++ myproject/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = 'django-insecure-9-0c*iuj*k&%*_if1w!)gam3l!@au79n4hi5w&771q125+aja@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'api.faran.pro'
+]
 
 
 # Application definition
